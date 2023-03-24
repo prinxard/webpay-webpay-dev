7a8001a3dd872b01f1238fc0f38c749f6024172a
prince unachukwu <prinxard@gmail.com>
2023-03-24 06:07:26 +0100

    update

--- src/components/Form/taxPayForm.js
+++ src/components/Form/taxPayForm.js
@@ -59,34 +59,39 @@ const NewPaymentForm = ({ res }) => {
     return (
       <>
         {isOpen && (
+          // <div className="fixed z-50 top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+          //   <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+          //   <div className="relative z-50 w-2/3 h-full bg-white p-4 rounded-lg shadow-lg">
+          //     <iframe
+          //       src={url}
+          //       allowFullScreen
+          //       className="w-full h-full lg:h-100vw border-0"
+          //     ></iframe>
+          //     <button
+          //       className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-800"
+          //       onClick={handleClose}
+          //     >
+          //       <svg
+          //         className="h-6 w-6"
+          //         fill="none"
+          //         viewBox="0 0 24 24"
+          //         stroke="currentColor"
+          //       >
+          //         <path
+          //           strokeLinecap="round"
+          //           strokeLinejoin="round"
+          //           strokeWidth={2}
+          //           d="M6 18L18 6M6 6l12 12"
+          //         />
+          //       </svg>
+          //     </button>
+          //   </div>
+          // </div>
           <div className="fixed z-50 top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-            <div className="relative z-50 w-2/3 h-full bg-white p-4 rounded-lg shadow-lg">
-              <iframe
-                src={url}
-                allowFullScreen
-                className="w-full h-full lg:h-100vw border-0"
-              ></iframe>
-              <button
-                className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-800"
-                onClick={handleClose}
-              >
-                {/* <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg> */}
-              </button>
-            </div>
+            <iframe src={url} style={{ width: '100%', height: '100vh', border: 'none' }}></iframe>
+
           </div>
+
         )}
       </>
     );
@@ -347,7 +352,7 @@ const NewPaymentForm = ({ res }) => {
       setLoading(true);
       // setDisabled(true);
 
-      let result = axios.get(`https://irs.kg.gov.ng/etaxwebpay/v3/api_v3/recordpayment.php?${queryParams}`);
+      let result = await axios.get(`https://irs.kg.gov.ng/etaxwebpay/v3/api_v3/recordpayment.php?${queryParams}`);
       handleModalOpen(`https://irs.kg.gov.ng/etaxwebpay/v3/api_v3/processpayment.php?paymentref=${globalRef}`)
 
       // if (data.paymentgateway === "Bank Branch") {
@@ -967,7 +972,6 @@ const NewPaymentForm = ({ res }) => {
 
         </>
       )}
-      {/* <Modal isOpen={isModalOpen} onClose={handleModalClose} url={modalUrl} /> */}
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleModalOpen}
